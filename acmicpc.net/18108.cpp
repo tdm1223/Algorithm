c3e710adf7d83cbe20129d4b4949aab592b72cb7
tdm1223 <tdm1223@naver.com>
2019-12-25 21:09:01 +0900

    [ADD] BOJ 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ acmicpc.net/18108.cpp
@@ -0,0 +1,14 @@
+// 18108. 1998년생인 내가 태국에서는 2541년생?!
+// 2019.12.25
+// 입문용
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n - 543 << endl;
+    return 0;
+}
