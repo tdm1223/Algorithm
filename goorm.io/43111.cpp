acac5cde85a1a0cb8bf5c6578be7f2a929b2699c
Jegyun Kang <tdm1223@naver.com>
2020-03-18 00:23:51 +0900

    [ADD] goorm 홀짝 판별

---
+++ goorm.io/43111.cpp
@@ -0,0 +1,20 @@
+// 홀짝 판별
+// 2020.03.18
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cin>>a;
+	if(a%2==1)
+	{
+		cout<<"odd"<<endl;
+	}
+	else
+	{
+		cout<<"even"<<endl;
+	}
+	return 0;
+}
