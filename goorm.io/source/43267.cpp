a829cc133d1c87d9f5ab1aa14c3d78cbbdaa5c45
Jegyun Kang <tdm1223@naver.com>
2020-04-03 21:04:03 +0900

    [ADD] goorm Hello Goorm !

---
+++ goorm.io/source/43267.cpp
@@ -0,0 +1,16 @@
+// Hello Goorm !
+// 2020.04.03
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Hello Goorm !" << endl;
+    }
+    return 0;
+}
