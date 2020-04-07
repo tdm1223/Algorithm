2ed85e50b81014e474d5ea3a4d94cc1ed63660d0
Jegyun Kang <tdm1223@naver.com>
2020-04-08 00:04:06 +0900

    [UPDATE] BOJ 1500 최대 곱

--- acmicpc.net/source/1500.cpp
+++ acmicpc.net/source/1500.cpp
@@ -1,36 +1,33 @@
 // 1500. 최대 곱
-// 2019.05.18
+// 2020.04.08
 // 수학
-// https://tdm1223.tistory.com/106
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-	long long s, k;
-	cin >> s >> k;
+    long long s, k;
+    cin >> s >> k;
 
-	long long a = s / k; // 몫
-	long long b = s % k; // 나머지
+    long long a = s / k; // 몫
+    long long b = s % k; // 나머지
 
-	long long ans = 1;
-
-	// 나머지 개수만큼 (몫+1)을 곱하고 남은건 몫만 곱한다.
-	while (k>0)
-	{
-		k--;
-		if (b > 0)
-		{
-			ans *= (a + 1);
-			b--;
-		}
-		else
-		{
-			ans *= a;
-		}
-	}
-
-	cout << ans << endl;
-	return 0;
+    long long ans = 1;
+    // 나머지 개수만큼 (몫+1)을 곱하고 남은건 몫만 곱한다.
+    while (k > 0)
+    {
+        k--;
+        if (b > 0)
+        {
+            ans *= (a + 1);
+            b--;
+        }
+        else
+        {
+            ans *= a;
+        }
+    }
+    scout << ans << endl;
+    return 0;
 }
