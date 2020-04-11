1605a3788975c42461cafb96ca3a46b5f57f8d48
Jegyun Kang <tdm1223@naver.com>
2020-04-11 22:43:19 +0900

    [UPDATE] BOJ 1406 에디터

--- acmicpc.net/source/1406.cpp
+++ acmicpc.net/source/1406.cpp
@@ -1,7 +1,6 @@
 // 1406. 에디터
-// 2019.05.18
+// 2020.04.11
 // 링크드 리스트
-// https://tdm1223.tistory.com/62
 #include<iostream>
 #include<string>
 #include<list>
@@ -10,58 +9,54 @@ using namespace std;
 
 int main()
 {
-	string s;
-	cin >> s;
+    string s;
+    cin >> s;
 
-	list<char> list;
-	for (int i = 0; i < s.size(); i++)
-	{
-		list.push_back(s[i]);
-	}
-	auto cur = list.end();
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		char order;
-		cin >> order;
-		switch (order)
-		{
-		// 커서를 왼쪽으로 한 칸 옮김
-		case 'L':
-			if (cur != list.begin()) // 맨 앞이면 무시
-			{
-				cur--;
-			}
-			break;
-		// 커서를 오른쪽으로 한 칸 옮김
-		case 'D':
-			if (cur != list.end()) // 맨 뒤면 무시
-			{
-				cur++;
-			}
-			break;
-		// 커서 왼쪽에 있는 문자 삭제
-		case 'B':
-			if (cur != list.begin()) // 맨 앞이면 무시
-			{
-				cur--;
-				cur = list.erase(cur);
-			}
-			break;
-		// P뒤에 나오는 문자를 커서 왼쪽에 추가함
-		case 'P':
-			char tmp;
-			cin >> tmp;
-			list.insert(cur, tmp);
-			break;
-		}
-	}
-	// 결과 출력
-	for (auto iter = list.begin(); iter != list.end(); iter++)
-	{
-		cout << *iter;
-	}
-	cout << endl;
-	return 0;
+    list<char> list;
+    for (int i = 0; i < s.size(); i++)
+    {
+        list.push_back(s[i]);
+    }
+    auto cur = list.end();
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        char order;
+        cin >> order;
+        switch (order)
+        {
+        case 'L': // 왼쪽으로
+            if (cur != list.begin())
+            {
+                cur--;
+            }
+            break;
+        case 'D': // 오른쪽으로
+            if (cur != list.end())
+            {
+                cur++;
+            }
+            break;
+        case 'B': // 왼쪽에 있는 문자 삭제
+            if (cur != list.begin())
+            {
+                cur--;
+                cur = list.erase(cur);
+            }
+            break;
+        case 'P': // 왼쪽에 문자 추가
+            char tmp;
+            cin >> tmp;
+            list.insert(cur, tmp);
+            break;
+        }
+    }
+
+    for (auto iter = list.begin(); iter != list.end(); iter++)
+    {
+        cout << *iter;
+    }
+    cout << endl;
+    return 0;
 }
