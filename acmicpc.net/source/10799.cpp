cdf0301f73f3bec005963fc8ee4a4368f1196aa3
Jegyun Kang <tdm1223@naver.com>
2020-04-15 14:35:32 +0900

    [UPDATE] BOJ 10799 쇠막대기

--- acmicpc.net/source/10799.cpp
+++ acmicpc.net/source/10799.cpp
@@ -1,7 +1,6 @@
 // 10799. 쇠막대기
-// 2019.05.22
+// 2020.04.15
 // 스택
-// https://tdm1223.tistory.com/63
 #include<iostream>
 #include<stack>
 #include<string>
@@ -10,31 +9,31 @@ using namespace std;
 
 int main()
 {
-	string s;
-	cin >> s;
-	stack<char> a;
-	int ans = 0;
-	for (int i = 0; i < s.size(); i++)
-	{
-		if (s[i] == '(')
-		{
-			a.push(s[i]);
-		}
-		else if (s[i] == ')')
-		{
-			a.pop();
-			// 레이저일땐 스택의 크기만큼 더함
-			if (s[i - 1] == '(')
-			{
-				ans += a.size();
-			}
-			else if (s[i - 1] == ')')
-			{
-				ans++;
-			}
-		}
-	}
-	// 결과 출력
-	cout << ans << endl;
-	return 0;
+    string s;
+    cin >> s;
+    stack<char> a;
+    int ans = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            a.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            a.pop();
+            // 레이저라면 스택의 크기만큼 더함
+            if (s[i - 1] == '(')
+            {
+                ans += a.size();
+            }
+            else if (s[i - 1] == ')')
+            {
+                ans++;
+            }
+        }
+    }
+    // 결과 출력
+    cout << ans << endl;
+    return 0;
 }
