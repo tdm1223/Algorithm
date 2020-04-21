6d5d702557f3edbdb213b65c8942729623836082
Jegyun Kang <tdm1223@naver.com>
2020-04-21 20:46:36 +0900

    [UPDATE] BOJ 1964 오각형, 오각형, 오각형...

--- acmicpc.net/source/1964.cpp
+++ acmicpc.net/source/1964.cpp
@@ -1,19 +1,18 @@
 // 1964. 오각형, 오각형, 오각형…
-// 2019.05.19
+// 2020.04.21
 // 수학
-// https://tdm1223.tistory.com/112
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-	long long n;
-	cin >> n;
-	// 각 항은 3n+4인 등차수열의 합만큼 갯수가 증가한다.
-	// n이 아닌 n-1항까지의 합을 계산한후 처음값인 5와 더한다.
-	// 등차수열의 합은 n*{2a + (n-1)d}/2로 계산. a는 첫째항, d는 공차
-	long long sum = (((n - 1)  * (14 + (n - 2) * 3)) / 2) % 45678;
-	cout << 5 + sum << endl;
-	return 0;
+    long long n;
+    cin >> n;
+    // 각 항은 3n+4인 등차수열의 합만큼 갯수가 증가한다.
+    // n이 아닌 n-1항까지의 합을 계산한후 처음값인 5와 더한다.
+    // 등차수열의 합은 n*{2a + (n-1)d}/2로 계산. a는 첫째항, d는 공차
+    long long sum = (((n - 1) * (14 + (n - 2) * 3)) / 2) % 45678;
+    cout << 5 + sum << endl;
+    return 0;
 }
