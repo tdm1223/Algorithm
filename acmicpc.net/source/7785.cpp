ee20ecbab3f7254946131368fa47daeded8976e1
Jegyun Kang <tdm1223@naver.com>
2020-05-16 22:56:29 +0900

    [UPDATE] BOJ 7785 회사에 있는 사람

--- acmicpc.net/source/7785.cpp
+++ acmicpc.net/source/7785.cpp
@@ -1,7 +1,6 @@
 // 7785. 회사에 있는 사람
-// 2019.05.21
+// 2020.05.16
 // 자료구조
-// https://tdm1223.tistory.com/116
 #include<iostream>
 #include<map>
 #include<string>
@@ -10,30 +9,30 @@ using namespace std;
 
 int main()
 {
-	map<string, bool> map; // string의 존재유무 저장 하는 map
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		string a, b;
-		cin >> a >> b;
-		if (b == "enter")
-		{
-			map[a] = true;
-		}
-		else
-		{
-			map[a] = false;
-		}
-	}
+    map<string, bool> map; // string의 존재유무 저장 하는 map
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string a, b;
+        cin >> a >> b;
+        if (b == "enter")
+        {
+            map[a] = true;
+        }
+        else
+        {
+            map[a] = false;
+        }
+    }
 
-	// 사전 역순으로 출력
-	for (auto iter = map.rbegin(); iter != map.rend(); iter++)
-	{
-		if (iter->second == true)
-		{
-			cout << iter->first << "\n";
-		}
-	}
-	return 0;
+    // 사전 역순으로 출력
+    for (auto iter = map.rbegin(); iter != map.rend(); iter++)
+    {
+        if (iter->second == true)
+        {
+            cout << iter->first << "\n";
+        }
+    }
+    return 0;
 }
