e99619c7aeef385df4799d37aa7abe47c5777989
Jegyun Kang <tdm1223@naver.com>
2020-05-25 21:06:49 +0900

    [UPDATE] BOJ 10409 서버

--- acmicpc.net/source/10409.cpp
+++ acmicpc.net/source/10409.cpp
@@ -1,7 +1,6 @@
 // 10409. 서버
-// 2019.05.22
+// 2020.05.25
 // 구현
-// https://tdm1223.tistory.com/100
 #include<iostream>
 #include<vector>
 
@@ -9,23 +8,23 @@ using namespace std;
 
 int main()
 {
-	int n, t;
-	cin >> n >> t;
-	vector<int> v(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i];
-	}
-	int ans = 0;
-	// FCFS(FIFO)
-	for (int i = 0; i < v.size(); i++)
-	{
-		if (v[i] <= t)
-		{
-			ans++;
-		}
-		t -= v[i];
-	}
-	cout << ans << endl;
-	return 0;
+    int n, t;
+    cin >> n >> t;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    int ans = 0;
+    // FCFS(FIFO)
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i] <= t)
+        {
+            ans++;
+        }
+        t -= v[i];
+    }
+    cout << ans << endl;
+    return 0;
 }
