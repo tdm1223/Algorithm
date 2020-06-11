2804d905f68163f664750239d1e79efe594d227a
Jegyun Kang <tdm1223@naver.com>
2020-06-12 00:28:17 +0900

    [UPDATE] BOJ 13549 숨바꼭질 3

--- acmicpc.net/source/13549.cpp
+++ acmicpc.net/source/13549.cpp
@@ -1,7 +1,6 @@
 // 13549. 숨바꼭질 3
-// 2019.05.22
+// 2020.06.12
 // BFS
-// https://tdm1223.tistory.com/93
 #include<iostream>
 #include<queue>
 
@@ -11,47 +10,47 @@ int sec[200001];
 bool visit[200001]; // 방문 유무 저장
 int main()
 {
-	int n, k;
-	cin >> n >> k;
-	visit[n] = true;
-	queue<int> q;
-	q.push(n);
-	while (!q.empty())
-	{
-		int now = q.front();
-		q.pop();
-		// 순간이동
-		if (now * 2 <= 200000)
-		{
-			if (visit[now * 2] == false)
-			{
-				q.push(now * 2);
-				visit[now * 2] = true;
-				sec[now * 2] = sec[now];
-			}
-		}
-		// x-1로 이동
-		if (now - 1 >= 0)
-		{
-			if (visit[now - 1] == false)
-			{
-				q.push(now - 1);
-				visit[now - 1] = true;
-				sec[now - 1] = sec[now] + 1;
-			}
-		}
-		// x+1로 이동
-		if (now + 1 <= 200000)
-		{
-			if (visit[now + 1] == false)
-			{
-				q.push(now + 1);
-				visit[now + 1] = true;
-				sec[now + 1] = sec[now] + 1;
-			}
-		}
-	}
-	// 결과 출력
-	cout << sec[k] << endl;
-	return 0;
+    int n, k;
+    cin >> n >> k;
+    visit[n] = true;
+    queue<int> q;
+    q.push(n);
+    while (!q.empty())
+    {
+        int now = q.front();
+        q.pop();
+        // 순간이동
+        if (now * 2 <= 200000)
+        {
+            if (visit[now * 2] == false)
+            {
+                q.push(now * 2);
+                visit[now * 2] = true;
+                sec[now * 2] = sec[now];
+            }
+        }
+        // x-1로 이동
+        if (now - 1 >= 0)
+        {
+            if (visit[now - 1] == false)
+            {
+                q.push(now - 1);
+                visit[now - 1] = true;
+                sec[now - 1] = sec[now] + 1;
+            }
+        }
+        // x+1로 이동
+        if (now + 1 <= 200000)
+        {
+            if (visit[now + 1] == false)
+            {
+                q.push(now + 1);
+                visit[now + 1] = true;
+                sec[now + 1] = sec[now] + 1;
+            }
+        }
+    }
+    // 결과 출력
+    cout << sec[k] << endl;
+    return 0;
 }
