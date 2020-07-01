fc728fbe7dd4811f38d2c80752aaf7e69a260099
Jegyun Kang <tdm1223@naver.com>
2020-07-01 23:28:02 +0900

    [UPDATE] BOJ 1620 나는야 포켓몬 마스터 이다솜

--- acmicpc.net/source/1620.cpp
+++ acmicpc.net/source/1620.cpp
@@ -1,5 +1,5 @@
 // 1620. 나는야 포켓몬 마스터 이다솜
-// 2019.05.18
+// 2020.07.01
 // 자료구조
 #include<iostream>
 #include<map>
@@ -12,31 +12,31 @@ map<string, int> map2; // 이름->번호
 char name[21]; // 포켓몬 이름
 int main()
 {
-	int n, m;
-	cin >> n >> m;
+    int n, m;
+    cin >> n >> m;
 
-	for (int i = 0; i < n; i++)
-	{
-		string s;
-		scanf("%s", name);
-		s = name;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        scanf("%s", name);
+        s = name;
 
-		map1[i + 1] = s; // 번호->이름 저장
-		map2[s] = i + 1; // 이름->번호 저장
-	}
+        map1[i + 1] = s; // 번호->이름 저장
+        map2[s] = i + 1; // 이름->번호 저장
+    }
 
-	for (int i = 0; i < m; i++)
-	{
-		scanf("%s", name);
-		if (isdigit(name[0])) // 번호일때 이름 출력
-		{
-			cout << map1[atoi(name)] << "\n";
-		}
-		else // 이름일때 번호 출력
-		{
-			string s = name;
-			cout << map2[s] << "\n";
-		}
-	}
-	return 0;
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%s", name);
+        if (isdigit(name[0])) // 번호일때 이름 출력
+        {
+            cout << map1[atoi(name)] << "\n";
+        }
+        else // 이름일때 번호 출력
+        {
+            string s = name;
+            cout << map2[s] << "\n";
+        }
+    }
+    return 0;
 }
