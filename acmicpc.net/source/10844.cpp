475ffd2d3a496711a54d7731f3add97545e318b2
Jegyun Kang <tdm1223@naver.com>
2020-07-07 19:14:14 +0900

    [UPDATE] BOJ 10844 쉬운 계단 수

--- acmicpc.net/source/10844.cpp
+++ acmicpc.net/source/10844.cpp
@@ -1,7 +1,6 @@
 // 10844. 쉬운 계단 수
-// 2019.05.22
+// 2020.07.07
 // 다이나믹 프로그래밍
-// https://tdm1223.tistory.com/78
 #include<iostream>
 
 using namespace std;
@@ -9,39 +8,39 @@ using namespace std;
 int d[101][10]; // d[i][j] : 끝이 j이고 길이가 i인 계단수
 int main()
 {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	for (int i = 1; i < 10; i++)
-	{
-		d[1][i] = 1; // 끝이 i이고 길이가 1인 계단수는 1개
-	}
+    for (int i = 1; i < 10; i++)
+    {
+        d[1][i] = 1; // 끝이 i이고 길이가 1인 계단수는 1개
+    }
 
-	for (int i = 2; i <= n; i++)
-	{
-		for (int j = 0; j <= 9; j++)
-		{
-			d[i][j] = 0;
-			if (j - 1 >= 0)
-			{
-				d[i][j] += d[i - 1][j - 1];
-			}
-			if (j + 1 <= 9)
-			{
-				d[i][j] += d[i - 1][j + 1];
-			}
-			d[i][j] %= 1000000000;
-		}
-	}
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 0; j <= 9; j++)
+        {
+            d[i][j] = 0;
+            if (j - 1 >= 0)
+            {
+                d[i][j] += d[i - 1][j - 1];
+            }
+            if (j + 1 <= 9)
+            {
+                d[i][j] += d[i - 1][j + 1];
+            }
+            d[i][j] %= 1000000000;
+        }
+    }
 
-	long long ans = 0;
-	// 길이가 n인 끝이 0~9인 계단수를 더함
-	for (int i = 0; i <= 9; i++)
-	{
-		ans += d[n][i];
-		ans %= 1000000000;
-	}
+    long long ans = 0;
+    // 길이가 n인 끝이 0~9인 계단수를 더함
+    for (int i = 0; i <= 9; i++)
+    {
+        ans += d[n][i];
+        ans %= 1000000000;
+    }
 
-	cout << ans << endl;
-	return 0;
+    cout << ans << endl;
+    return 0;
 }
