bfe2b7f401c77dc9ab571398f55cf93a12cf1cab
Jegyun Kang <tdm1223@naver.com>
2020-07-26 15:00:09 +0900

    [UPDATE] BOJ 2193 이친수

--- acmicpc.net/source/2193.cpp
+++ acmicpc.net/source/2193.cpp
@@ -1,7 +1,6 @@
 // 2193. 이친수
-// 2019.05.19
+// 2020.07.26
 // 다이나믹 프로그래밍
-// https://tdm1223.tistory.com/79
 #include<iostream>
 
 using namespace std;
@@ -9,15 +8,15 @@ using namespace std;
 long long d[91]; // d[n] : n자리 이친수의 개수
 int main()
 {
-	int n;
-	cin >> n;
-	d[1] = 1; // 1
-	d[2] = 1; // 10
-	// 점화식을 작성해보면 1,1,2,3,5,8개 순으로 이친수의 개수가 증가하게 된다.
-	for (int i = 3; i <= n; i++)
-	{
-		d[i] = d[i - 1] + d[i - 2];
-	}
-	cout << d[n] << endl;
-	return 0;
+    int n;
+    cin >> n;
+    d[1] = 1; // 1
+    d[2] = 1; // 10
+    // 점화식을 작성해보면 1,1,2,3,5,8개 순으로 이친수의 개수가 증가하게 된다.
+    for (int i = 3; i <= n; i++)
+    {
+        d[i] = d[i - 1] + d[i - 2];
+    }
+    cout << d[n] << endl;
+    return 0;
 }
