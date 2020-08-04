74b0bd794ee21a3c14f3cd8b225674e8252dc37b
Jegyun Kang <tdm1223@naver.com>
2020-08-04 23:45:17 +0900

    [UPDATE] BOJ 1699 제곱수의 합

--- acmicpc.net/source/1699.cpp
+++ acmicpc.net/source/1699.cpp
@@ -1,7 +1,6 @@
 // 1699. 제곱수의 합
-// 2019.05.18
+// 2020.08.04
 // 다이나믹 프로그래밍
-// https://tdm1223.tistory.com/80
 #include<iostream>
 #include<algorithm>
 #include<cmath>
@@ -11,21 +10,20 @@ using namespace std;
 int d[100001]; // d[i] : i를 제곱수의 합으로 나타내는 방법의 최소값
 int main()
 {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	for (int i = 1; i <= n; i++)
-	{
-		d[i] = i; // 일종의 최댓값. 모두 1+1+... 로 구성하는것
-		for (int j = 1; j*j <= i; j++)
-		{
-			if (d[i] > d[i - j * j] + 1)
-			{
-				d[i] = d[i - j * j] + 1;
-			}
-		}
-	}
-
-	cout << d[n] << endl;
-	return 0;
+    for (int i = 1; i <= n; i++)
+    {
+        d[i] = i; // 일종의 최댓값. 모두 1+1+... 로 구성하는것
+        for (int j = 1; j * j <= i; j++)
+        {
+            if (d[i] > d[i - j * j] + 1)
+            {
+                d[i] = d[i - j * j] + 1;
+            }
+        }
+    }
+    cout << d[n] << endl;
+    return 0;
 }
