06543d6af7f1a0b9390db044680ba34132302835
Jegyun Kang <tdm1223@naver.com>
2020-09-21 01:09:08 +0900

    [UPDATE] BOJ 11727 2xn 타일링 2

--- acmicpc.net/source/11727.cpp
+++ acmicpc.net/source/11727.cpp
@@ -1,7 +1,6 @@
 // 11727. 2×n 타일링 2
-// 2019.05.22
+// 2020.09.21
 // 다이나믹 프로그래밍
-// https://tdm1223.tistory.com/83
 #include<iostream>
 
 using namespace std;
@@ -9,15 +8,15 @@ using namespace std;
 int d[1001]; // d[i] : 2xi크기의 직사각형을 채우는 방법의 수를 10007로 나눈 나머지
 int main()
 {
-	int n;
-	cin >> n;
-	d[1] = 1;
-	d[2] = 3;
-	for (int i = 3; i <= n; i++)
-	{
-		d[i] = d[i - 1] + 2 * d[i - 2]; // 2x2정사각형도 있으므로 2*d[i-2]
-		d[i] %= 10007;
-	}
-	cout << d[n] << endl;
-	return 0;
+    int n;
+    cin >> n;
+    d[1] = 1;
+    d[2] = 3;
+    for (int i = 3; i <= n; i++)
+    {
+        d[i] = d[i - 1] + 2 * d[i - 2]; // 2x2정사각형도 있으므로 2*d[i-2]
+        d[i] %= 10007;
+    }
+    cout << d[n] << endl;
+    return 0;
 }
