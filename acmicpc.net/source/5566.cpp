51aade62453b72d7e4052461fe7e84f269824d2e
Jegyun Kang <tdm1223@naver.com>
2020-10-03 14:00:56 +0900

    [UPDATE] BOJ 5566 주사위 게임

--- acmicpc.net/source/5566.cpp
+++ acmicpc.net/source/5566.cpp
@@ -1,7 +1,6 @@
 // 5566. 주사위 게임
-// 2019.05.21
+// 2020.10.03
 // 시뮬레이션
-// https://tdm1223.tistory.com/101
 #include<iostream>
 #include<vector>
 
@@ -11,30 +10,30 @@ int map[1001];
 int order[1001];
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	int cnt = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> map[i];
-	}
-	int ans = 0;
-	for (int i = 1; i <= m; i++)
-	{
-		cin >> order[i];
-	}
-	// 순서대로 주사위 확인
-	for (int i = 1; i <= m; i++)
-	{
-		cnt += order[i]; // 주사위에 해당하는 수만큼 이동
-		cnt += map[cnt]; // 지시에 해당하는 수만큼 이동
-		// 맵을 벗어나면 종료
-		if (cnt >= n)
-		{
-			ans = i;
-			break;
-		}
-	}
-	cout << ans << endl;
-	return 0;
+    int n, m;
+    cin >> n >> m;
+    int cnt = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> map[i];
+    }
+    int ans = 0;
+    for (int i = 1; i <= m; i++)
+    {
+        cin >> order[i];
+    }
+    // 순서대로 주사위 확인
+    for (int i = 1; i <= m; i++)
+    {
+        cnt += order[i]; // 주사위에 해당하는 수만큼 이동
+        cnt += map[cnt]; // 지시에 해당하는 수만큼 이동
+        // 맵을 벗어나면 종료
+        if (cnt >= n)
+        {
+            ans = i;
+            break;
+        }
+    }
+    cout << ans << endl;
+    return 0;
 }
