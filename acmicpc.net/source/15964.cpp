43c08b42227b4dd0275d46bec89df6ed1ba189b0
Jegyun Kang <tdm1223@naver.com>
2020-10-09 00:08:33 +0900

    [ADD] BOJ 15964 이상한 기호

---
+++ acmicpc.net/source/15964.cpp
@@ -0,0 +1,14 @@
+// 15964. 이상한 기호
+// 2020.10.09
+// 수학
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    cout << (a + b) * (a - b) << endl;
+    return 0;
+}
