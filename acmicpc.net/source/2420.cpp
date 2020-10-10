522844c6d9e6ba4e98d0938ab8c858b09b64559c
Jegyun Kang <tdm1223@naver.com>
2020-10-10 17:58:53 +0900

    [ADD] BOJ 2420 사라피월드
    - 수학

---
+++ acmicpc.net/source/2420.cpp
@@ -0,0 +1,15 @@
+// 2420. 사파리월드
+// 2020.10.10
+// 수학
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    cout << abs(a - b) << endl;
+    return 0;
+}
