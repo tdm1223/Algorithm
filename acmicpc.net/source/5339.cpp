420e90bf434de7cd77ba5039e60562c3a7f132b4
Jegyun Kang <tdm1223@naver.com>
2020-10-11 17:06:42 +0900

    [ADD] BOJ 5339 콜센터
    - 구현

---
+++ acmicpc.net/source/5339.cpp
@@ -0,0 +1,21 @@
+// 5339. 콜센터
+// 2020.10.11
+// 구현
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "     /~\\" << endl;
+    cout << "    ( oo|" << endl;
+    cout << "    _\\=/_" << endl;
+    cout << "   /  _  \\" << endl;
+    cout << "  //|/.\\|\\\\" << endl;
+    cout << " ||  \\ /  ||" << endl;
+    cout << "============" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+    return 0;
+}
