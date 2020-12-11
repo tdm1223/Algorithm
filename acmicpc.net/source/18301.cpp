c802de78dd62ec88b516ea8a899142fa62d85915
Jegyun Kang <tdm1223@naver.com>
2020-12-12 01:21:36 +0900

    [ADD] BOJ 18301 Rats
    - 입문용

---
+++ acmicpc.net/source/18301.cpp
@@ -0,0 +1,14 @@
+// 18301. Rats
+// 2020.12.12
+// 입문용, 사칙연산
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (a + 1) * (b + 1) / (c + 1) - 1 << endl;
+    return 0;
+}
