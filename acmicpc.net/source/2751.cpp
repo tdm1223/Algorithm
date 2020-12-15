5614d54851d6f1e654ac3b56eb7d76ba8d382763
Jegyun Kang <tdm1223@naver.com>
2020-12-15 23:12:03 +0900

    [UPDATE] BOJ 2751 수 정렬하기 2
    - 정렬

--- acmicpc.net/source/2751.cpp
+++ acmicpc.net/source/2751.cpp
@@ -1,59 +1,27 @@
 // 2751. 수 정렬하기 2
-// 2019.05.20
+// 2020.12.15
 // 정렬
-#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
-#define SWAP(x, y) { int t = x; x = y; y = t; }
-int n;
-int arr[1000000];
-
-void MySort(int *array, int left, int right)
-{
-	int mLeft = left, mRight = right;
-	int pivot = array[(left + right) / 2];
-
-	while (mLeft <= mRight)
-	{
-		while (pivot > array[mLeft])
-		{
-			mLeft++;
-		}
-		while (pivot < array[mRight])
-		{
-			mRight--;
-		}
-
-		if (mLeft <= mRight)
-		{
-			SWAP(array[mLeft], array[mRight]);
-			mLeft++;
-			mRight--;
-		}
-	}
-	if (left < mRight)
-	{
-		MySort(arr, left, mRight);
-	}
-	if (mLeft < right)
-	{
-		MySort(arr, mLeft, right);
-	}
-}
+using namespace std;
 
 int main()
 {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
+    int n;
+    scanf("%d", &n);
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &v[i]);
+    }
 
-	MySort(arr, 0, n - 1);
+    sort(v.begin(), v.end());
 
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-
-	return 0;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", v[i]);
+    }
+    return 0;
 }
