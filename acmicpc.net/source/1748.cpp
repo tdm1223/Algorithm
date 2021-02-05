8e0da86476b873d2e1d39af788e6e2981d3e6ee5
Jegyun Kang <tdm1223@naver.com>
2021-02-05 21:38:03 +0900

    [UPDATE] BOJ 1748 수 이어쓰기 1
    - 재채점으로 인한 다시 풀이

--- acmicpc.net/source/1748.cpp
+++ acmicpc.net/source/1748.cpp
@@ -1,26 +1,29 @@
 // 1748. 수 이어쓰기 1
-// 2019.05.18
+// 2021.02.05
 // 브루트 포스, 구현
 #include<iostream>
+#include<string>
+#include<cmath>
 
 using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
-	int count = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		count++;
-		int k = i;
-		while (k>=10) // 자릿수를 더함
-		{
-			k = k/10;
-			count++;
-		}
-	}
-
-	cout << count << endl;
-	return 0;
+    string n;
+    cin >> n;
+    if (n.size() == 1)
+    {
+        cout << n << endl;
+    }
+    else
+    {
+        int answer = 0;
+        for (int i = 1; i < n.size(); i++)
+        {
+            answer += i * (9 * pow(10, i - 1));
+        }
+        answer += n.size() * (stoi(n) - pow(10, n.size() - 1) + 1);
+        cout << answer << endl;
+    }
+    return 0;
 }
