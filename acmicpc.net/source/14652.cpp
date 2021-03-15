16841a1125234841b8c88b1d656b357527013840
Jegyun Kang <tdm1223@naver.com>
2021-03-15 19:52:25 +0900

    [ADD] BOJ 14652 나는 행복합니다~
    - 수학

---
+++ acmicpc.net/source/14652.cpp
@@ -0,0 +1,14 @@
+// 14652. 나는 행복합니다~
+// 2021.03.15
+// 수학
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << k / m << " " << k % m << endl;
+    return 0;
+}
