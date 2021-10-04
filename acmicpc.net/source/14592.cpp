5572f00458c37bea3f87f3418ff472b04e0a2462
Jegyun Kang <tdm1223@naver.com>
2021-10-05 01:01:30 +0900

    [ADD] BOJ 14592 2017 아주대학교 프로그래밍 경시대회 (Small)
    - 구현

---
+++ acmicpc.net/source/14592.cpp
@@ -0,0 +1,52 @@
+// 14592. 2017 아주대학교 프로그래밍 경시대회 (Small)
+// 2021.10.05
+// 구현
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int s, c, l;
+    int maxS = 0;
+    int minC = 0;
+    int minL = 0;
+    int maxIdx = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> s >> c >> l;
+
+        if (maxS < s)
+        {
+            maxIdx = i;
+            maxS = s;
+            minC = c;
+            minL = l;
+        }
+        else if (maxS == s)
+        {
+            if (minC == c)
+            {
+                if (minL > l)
+                {
+                    maxIdx = i;
+                    maxS = s;
+                    minC = c;
+                    minL = l;
+                }
+            }
+            else if (minC > c)
+            {
+                maxIdx = i;
+                maxS = s;
+                minC = c;
+                minL = l;
+            }
+        }
+    }
+    cout << maxIdx << endl;
+    return 0;
+}
