1d85920b9a977ed21aa3d37327bafd4c90692334
Jegyun Kang <tdm1223@naver.com>
2021-10-17 19:46:27 +0900

    [UPDATE] BOJ 1672 DNA 해독
    - 구현

--- acmicpc.net/source/1672.cpp
+++ acmicpc.net/source/1672.cpp
@@ -1,102 +1,30 @@
 // 1672. DNA 해독
-// 2020.10.25
+// 2021.10.17
 // 구현
 #include<iostream>
 #include<string>
 
 using namespace std;
 
-char Mapping(char x, char y)
+int n;
+string DNA = "ACAGCGTAATCGGAGT";
+
+char Mapping(char a, char b)
 {
-    char ans;
-    switch (x)
-    {
-    case 'A':
-        switch (y)
-        {
-        case 'A':
-            ans = 'A';
-            break;
-        case 'G':
-            ans = 'C';
-            break;
-        case 'C':
-            ans = 'A';
-            break;
-        case 'T':
-            ans = 'G';
-            break;
-        }
-        break;
-    case 'G':
-        switch (y)
-        {
-        case 'A':
-            ans = 'C';
-            break;
-        case 'G':
-            ans = 'G';
-            break;
-        case 'C':
-            ans = 'T';
-            break;
-        case 'T':
-            ans = 'A';
-            break;
-        }
-        break;
-    case 'C':
-        switch (y)
-        {
-        case 'A':
-            ans = 'A';
-            break;
-        case 'G':
-            ans = 'T';
-            break;
-        case 'C':
-            ans = 'C';
-            break;
-        case 'T':
-            ans = 'G';
-            break;
-        }
-        break;
-    case 'T':
-        switch (y)
-        {
-        case 'A':
-            ans = 'G';
-            break;
-        case 'G':
-            ans = 'A';
-            break;
-        case 'C':
-            ans = 'G';
-            break;
-        case 'T':
-            ans = 'T';
-            break;
-        }
-        break;
-    }
-    return ans;
+    int idx = (a == 'A' ? 0 : a == 'G' ? 1 : a == 'C' ? 2 : 3);
+    int idx2 = (b == 'A' ? 0 : b == 'G' ? 1 : b == 'C' ? 2 : 3);
+
+    return DNA[idx * 4 + idx2];
 }
+
 int main()
 {
-    int n;
-    cin >> n;
-    n--;
-    string dna;
-    cin >> dna;
-    while (n-- > 0)
+    string s;
+    cin >> n >> s;
+    for (int i = s.size() - 2; i >= 0; i--)
     {
-        char x = dna[dna.size() - 1];
-        char y = dna[dna.size() - 2];
-
-        dna = dna.substr(0, dna.size() - 2);
-        dna += Mapping(x, y);
+        s[i] = Mapping(s[i], s[i + 1]);
     }
-    cout << dna << endl;
+    cout << s[0] << "\n";
     return 0;
 }
