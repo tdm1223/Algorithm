d5c8f116aa36ae4adbc835dfba4f7ee113cb2182
Jegyun Kang <tdm1223@naver.com>
2021-12-02 00:03:49 +0900

    [ADD] BOJ 15700 타일 채우기 4
    - 수학

---
+++ acmicpc.net/source/15700.cpp
@@ -0,0 +1,15 @@
+// 15700. 타일 채우기 4
+// 2021.12.02
+// 수학
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+
+    cout << (n * m) / 2 << endl;
+    return 0;
+}
