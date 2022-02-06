2daa0ba017dfb14a79dba62997d3ee9c9001db36
Jegyun Kang <tdm1223@naver.com>
2022-02-06 23:47:58 +0900

    [ADD] BOJ 24264 알고리즘 수업 - 알고리즘의 수행 시간 3
    - 구현

---
+++ acmicpc.net/source/24264.cpp
@@ -0,0 +1,16 @@
+// 24264. 알고리즘 수업 - 알고리즘의 수행 시간 3
+// 2022.02.06
+// 구현
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    auto ans = n * n;
+    cout << ans << endl << 2 << endl;
+    return 0;
+}
