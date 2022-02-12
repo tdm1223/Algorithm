d4d91bffa84017c16292d03e0b560ed58875b1d5
Jegyun Kang <tdm1223@naver.com>
2022-02-12 23:45:46 +0900

    [ADD] BOJ 24266 알고리즘 수업 - 알고리즘의 수행 시간 5
    - 구현

---
+++ acmicpc.net/source/24266.cpp
@@ -0,0 +1,14 @@
+// 24266. 알고리즘 수업 - 알고리즘의 수행 시간 5
+// 2022.02.12
+// 구현
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    cout << n * n * n << endl << 3 << endl;
+    return 0;
+}
