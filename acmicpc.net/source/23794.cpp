c4f665f5a26f956aa86b3706e6950b3eed95be70
Jegyun Kang <tdm1223@naver.com>
2022-02-13 18:14:33 +0900

    [ADD] BOJ 23794 골뱅이 찍기 - 정사각형
    - 출력

---
+++ acmicpc.net/source/23794.cpp
@@ -0,0 +1,39 @@
+// 23794 골뱅이 찍기 - 정사각형
+// 2022.02.13
+// 출력
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n + 2; i++)
+    {
+        if (i == 1 or i == n + 2)
+        {
+            for (int j = 1; j <= n + 2; j++)
+            {
+                cout << "@";
+            }
+        }
+        else
+        {
+            for (int j = 1; j <= n + 2; j++)
+            {
+                if (j == 1 or j == n + 2)
+                {
+                    cout << "@";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
