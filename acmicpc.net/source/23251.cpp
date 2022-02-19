3ac3a771aa7d674394c54dd22aaf09cfb954b894
Jegyun Kang <tdm1223@naver.com>
2022-02-20 01:31:26 +0900

    [ADD] BOJ 23251 스물셋
    - 수학

---
+++ acmicpc.net/source/23251.cpp
@@ -0,0 +1,21 @@
+// 23251. 스물셋
+// 2022.02.20
+// 수학
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t, k;
+    cin >> t;
+    while (t-- > 0)
+    {
+        cin >> k;
+        cout << 23 * k << "\n";
+    }
+    return 0;
+}
