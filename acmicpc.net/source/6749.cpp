2afe86b8a38b2274c93a8d2701afb56bbad04bef
Jegyun Kang <tdm1223@naver.com>
2022-02-23 20:36:06 +0900

    [ADD] BOJ 6749 Next in line
    - 수학

---
+++ acmicpc.net/source/6749.cpp
@@ -0,0 +1,14 @@
+// 6749. Next in line
+// 2022.02.23
+// 수학
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << b + (b - a) << endl;
+    return 0;
+}
