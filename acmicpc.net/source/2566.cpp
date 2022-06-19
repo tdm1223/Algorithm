b5a2f9fe43930d855a8420336d79b37a2b638fef
Jegyun Kang <tdm1223@naver.com>
2022-06-19 18:42:06 +0900

    [UPDATE] BOJ 2566 최댓값
    - 입문용

--- acmicpc.net/source/2566.cpp
+++ acmicpc.net/source/2566.cpp
@@ -1,5 +1,5 @@
 // 2566. 최댓값
-// 2019.08.23
+// 2022.06.19
 // 입문용
 #include<iostream>
 
@@ -8,22 +8,22 @@ using namespace std;
 int map[10][10];
 int main()
 {
-	int max = 0;
-	int x = -1;
-	int y = -1;
-	for (int i = 1; i <= 9; i++)
-	{
-		for (int j = 1; j <= 9; j++)
-		{
-			cin >> map[i][j];
-			if (map[i][j] > max)
-			{
-				max = map[i][j];
-				x = i;
-				y = j;
-			}
-		}
-	}
-	cout << max << endl << x << " " << y << endl;
-	return 0;
+    int max = -1;
+    int x = -1;
+    int y = -1;
+    for (int i = 1; i <= 9; i++)
+    {
+        for (int j = 1; j <= 9; j++)
+        {
+            cin >> map[i][j];
+            if (map[i][j] > max)
+            {
+                max = map[i][j];
+                x = i;
+                y = j;
+            }
+        }
+    }
+    cout << max << endl << x << " " << y << endl;
+    return 0;
 }
