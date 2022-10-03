9d99b9f341c6ec97d0a8fa239655ed82c904bc70
Jegyun Kang <tdm1223@naver.com>
2022-10-03 15:01:01 +0900

    [ADD] BOJ 25628 햄버거 만들기
    - 수학

---
+++ acmicpc.net/source/25628.cpp
@@ -0,0 +1,15 @@
+// 25628. 햄버거 만들기
+// 2022.10.03
+// 수학
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << min(a / 2, b) << endl;
+    return 0;
+}
