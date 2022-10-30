eb93384493b76dcb6a1a9a20a9c7d7d01fe09ddc
Jegyun Kang <tdm1223@naver.com>
2022-10-30 19:29:19 +0900

    [ADD] BOJG 25314 코딩은 체육과목 입니다
    - 구현

---
+++ acmicpc.net/source/25314.cpp
@@ -0,0 +1,18 @@
+// 25314. 코딩은 체육과목 입니다
+// 2022.10.30
+// 구현
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n / 4; i++)
+    {
+        cout << "long ";
+    }
+    cout << "int" << endl;
+    return 0;
+}
