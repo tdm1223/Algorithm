c773ab0338e5a606ad0fc7d8989b0ee7cc1bf3fa
Jegyun Kang <tdm1223@naver.com>
2022-10-30 20:00:54 +0900

    [ADD] BOJ 20944 팰린드롬 척화비
    - 문자열

---
+++ acmicpc.net/source/20944.cpp
@@ -0,0 +1,18 @@
+// 20944. 팰린드롬 척화비
+// 2022.10.30
+// 문자열
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << 'a';
+    }
+    cout << endl;
+    return 0;
+}
