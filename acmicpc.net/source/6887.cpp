7fdd641ec8addb100585b6fc2ce58813f3343e8b
Jegyun Kang <tdm1223@naver.com>
2023-09-23 21:22:22 +0900

    [ADD] BOJ 6887 Squares
    - 수학

---
+++ acmicpc.net/source/6887.cpp
@@ -0,0 +1,16 @@
+// 6887. Squares
+// 2023.09.23
+// 수학
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    long long a;
+    cin >> a;
+
+    cout << "The largest square has side length " << floor(sqrt(a)) << "." << endl;
+    return 0;
+}
